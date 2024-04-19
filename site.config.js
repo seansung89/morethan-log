786c87e44f46f0fdf9e69fa4503e79f597f3fe94
seansung89 <167503923+seansung89@users.noreply.github.com>
2024-04-19 16:17:10 +0900

    Update site.config.js
    CONFIG configration
    delete link after v=

--- site.config.js
+++ site.config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://www.notion.so/5949574ed4b948d7a4b260eb2f3f433c?v=e18fb345570d4329abf4d56849a73770",
+  link: "https://www.notion.so/5949574ed4b948d7a4b260eb2f3f433c",
   since: 2024, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
